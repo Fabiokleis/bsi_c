2505a533203be908822a5f0c1175be899a6926dc
fabiokleis <fabiohkrc@gmail.com>
2021-08-12 18:03:57 -0300

    semana_9.1 e02.c feito

--- semana_9.1/e02.c
+++ semana_9.1/e02.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main(){
-    int i;
-    char *t;
-    char str[100];
-    char exp[3] = "ANA";
-    fgets(str, 100, stdin);
+    int i, j=0, p=0, c=0; 
+    char str[] = "ANA e MARIANA GOSTAM DE BANANA";
+    char exp[] = "ANA";
     
     for(i=0; str[i] != '\0'; i++){
-    
+        if(str[i] == exp[j]){
+            j++;
+            if(exp[j+1] == '\0'){
+                c++;
+                j = 0;
+            }
+        }else{
+            j = 0;
+        }
     }
 
+    printf("numero de ocorrencias da palavra ana: %d\n", c);
     return 0;
 }
+
+
